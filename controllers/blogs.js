90329c332bb78a346b3ff65e9709ad0edb4595d3
Vincent Marklynn <56606238+vmarklynn@users.noreply.github.com>
2024-07-11 22:33:48 -0700

    Separated routers to its own file

---
+++ controllers/blogs.js
@@ -0,0 +1,29 @@
+const blogsRouter = require('express').Router()
+const Blog = require('../models/blog')
+
+blogsRouter.get('/', (request, response) => {
+  Blog
+    .find({})
+    .then(blogs => {
+      response.json(blogs)
+    })
+})
+
+blogsRouter.post('/', (request, response) => {
+  const body = request.body
+
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes
+  })
+
+  blog
+    .save()
+    .then(result => {
+      response.status(201).json(result)
+    })
+})
+
+module.exports = blogsRouter
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 
-const Blog = require('./models/blog')
 const express = require('express')
+const blogsRouter = require('./controllers/blogs')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
@@ -17,30 +17,7 @@ mongoose.connect(mongoUrl).then(result => {
 app.use(cors())
 app.use(express.json())
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const body = request.body
-
-  const blog = new Blog({
-    title: body.title,
-    author: body.author,
-    url: body.url,
-    likes: body.likes
-  })
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+app.use('/api/blogs', blogsRouter)
 
 const PORT = process.env.PORT
 app.listen(PORT || 3001, () => {
