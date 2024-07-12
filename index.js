07876a77bb50152aa9c48080503d46bb732c30c4
Vincent Marklynn <56606238+vmarklynn@users.noreply.github.com>
2024-07-11 22:40:12 -0700

    Separated env information to its own file

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-require('dotenv').config()
-
+const { MONGODB_URI, PORT } = require('./utils/config')
 const express = require('express')
 const blogsRouter = require('./controllers/blogs')
 const app = express()
@@ -7,8 +6,7 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 
 
-const mongoUrl = process.env.MONGODB_URI
-mongoose.connect(mongoUrl).then(result => {
+mongoose.connect(MONGODB_URI).then(result => {
   console.log('Connected to MongoDB')
 }).catch(error => {
   console.log('Connection error: ', error.message)
@@ -19,7 +17,6 @@ app.use(express.json())
 
 app.use('/api/blogs', blogsRouter)
 
-const PORT = process.env.PORT
 app.listen(PORT || 3001, () => {
   console.log(`Server running on port ${PORT}`)
 })
