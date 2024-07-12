ad459ea07fe544450ae1018a9feb640c060313da
Vincent Marklynn <56606238+vmarklynn@users.noreply.github.com>
2024-07-11 23:14:33 -0700

    Added app.js to setup main express module

--- index.js
+++ index.js
@@ -1,27 +1,6 @@
-const { MONGODB_URI, PORT } = require('./utils/config')
-const { requestLogger, unknownEndpoint, validationError } = require('./utils/middleware.js')
-const { info, error } = require('./utils/logger')
-const express = require('express')
-const blogsRouter = require('./controllers/blogs')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
-
-
-mongoose.connect(MONGODB_URI).then(result => {
-  info('Connected to MongoDB')
-}).catch(error => {
-  error('Connection error: ', error.message)
-})
-
-app.use(cors())
-app.use(express.json())
-app.use(requestLogger)
-
-app.use('/api/blogs', blogsRouter)
-
-app.use(validationError)
-app.use(unknownEndpoint)
+const app = require('./app')
+const { PORT } = require('./utils/config')
+const { info } = require('./utils/logger')
 
 app.listen(PORT || 3001, () => {
   info(`Server running on port ${PORT}`)
