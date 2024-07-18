de9893a5ad959a3a9b45dcfedcf0c22b18276ad8
Vincent Marklynn <56606238+vmarklynn@users.noreply.github.com>
2024-07-18 08:30:09 -0700

    Added test to verify POST requests

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -13,10 +13,10 @@ blogsRouter.post('/', async (request, response, next) => {
     title: body.title,
     author: body.author,
     url: body.url,
-    likes: body.likes
+    likes: body.likes || 0
   })
 
-  const res = blog.save()
+  const res = await blog.save()
 
   response.status(201).json(res)
 
--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -1,4 +1,4 @@
-const { test, after, beforeEach } = require('node:test')
+const { test, after, beforeEach, describe } = require('node:test')
 const Blog = require('../models/blog')
 const assert = require('node:assert')
 const mongoose = require('mongoose')
@@ -24,48 +24,18 @@ const blogs = [
     url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
     likes: 5,
     __v: 0
-  },
-  {
-    _id: "5a422b3a1b54a676234d17f9",
-    title: "Canonical string reduction",
-    author: "Edsger W. Dijkstra",
-    url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
-    likes: 12,
-    __v: 0
-  },
-  {
-    _id: "5a422b891b54a676234d17fa",
-    title: "First class tests",
-    author: "Robert C. Martin",
-    url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
-    likes: 10,
-    __v: 0
-  },
-  {
-    _id: "5a422ba71b54a676234d17fb",
-    title: "TDD harms architecture",
-    author: "Robert C. Martin",
-    url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
-    likes: 0,
-    __v: 0
-  },
-  {
-    _id: "5a422bc61b54a676234d17fc",
-    title: "Type wars",
-    author: "Robert C. Martin",
-    url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
-    likes: 2,
-    __v: 0
   }
 ]
 
+
 beforeEach(async () => {
   await Blog.deleteMany({})
-  for (const blog of blogs) {
-    let blogObj = new Blog(blog)
-    await blogObj.save()
-  }
 
+  let blogObj = new Blog(blogs[0])
+  await blogObj.save()
+
+  blogObj = new Blog(blogs[1])
+  await blogObj.save()
 })
 
 test('blogs are returned as json', async () => {
@@ -77,12 +47,13 @@ test('blogs are returned as json', async () => {
 
 test('blogs return the correct number of blogs', async () => {
   const response = await api.get('/api/blogs')
-  assert.strictEqual(response.body.length, 6)
+  assert.strictEqual(response.body.length, blogs.length)
 })
 
 test('blogs have the field id as identifiers', async () => {
   const response = await api.get('/api/blogs')
   const firstBlog = response.body[0]
+  console.log(firstBlog)
   assert.strictEqual('id' in firstBlog, true)
   assert.strictEqual(!('_id' in firstBlog), true)
 })
@@ -103,11 +74,28 @@ test('POST requests should work as expected', async () => {
     .expect('Content-Type', /application\/json/)
 
   // TODO: Refactor to helpers
-  const blogsInDb = await Blog.find({})
-  assert.strictEqual(blogsInDb.length, blogs.length + 1)
+  const response = await api.get('/api/blogs')
+  assert.strictEqual(response.body.length, blogs.length + 1)
 })
 
-test('POST requests increase the number of ')
+test('POST requests with empty like will default to 0 ', async () => {
+  const testPost =
+  {
+    title: "Quit School",
+    author: "Primeagen",
+    url: "www.primeagen.com",
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(testPost)
+    .expect(201)
+
+  const response = await api.get('/api/blogs')
+
+  assert.strictEqual('likes' in response.body[blogs.length], true)
+  assert.strictEqual(response.body[blogs.length].likes, 0)
+})
 
 after(async () => {
   await mongoose.connection.close()
