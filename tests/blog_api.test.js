07b144c2bcff32b00a4685963b9418564daf7f99
Vincent Marklynn <56606238+vmarklynn@users.noreply.github.com>
2024-07-19 23:47:56 -0700

    Refactored tests by adding describes

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -8,95 +8,120 @@ const app = require('../app')
 
 const api = supertest(app)
 
-beforeEach(async () => {
-  await Blog.deleteMany({})
+describe('when there are existing notes', () => {
 
-  let blogObj = new Blog(initialBlogs[0])
-  await blogObj.save()
+  beforeEach(async () => {
+    await Blog.deleteMany({})
+    await Blog.insertMany(initialBlogs)
+  })
 
-  blogObj = new Blog(initialBlogs[1])
-  await blogObj.save()
-})
+  test('blogs are returned as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
 
-test('blogs are returned as json', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
-})
+  test('blogs return the correct number of blogs', async () => {
+    const response = await api.get('/api/blogs')
+    assert.strictEqual(response.body.length, initialBlogs.length)
+  })
 
-test('blogs return the correct number of blogs', async () => {
-  const response = await api.get('/api/blogs')
-  assert.strictEqual(response.body.length, initialBlogs.length)
-})
+  describe('blogs _id field gets replaced', () => {
 
-test('blogs have the field id as identifiers', async () => {
-  const response = await api.get('/api/blogs')
-  const firstBlog = response.body[0]
+    test('with id as identifiers', async () => {
+      const response = await api.get('/api/blogs')
+      const firstBlog = response.body[0]
 
-  assert.strictEqual('id' in firstBlog, true)
-  assert.strictEqual(!('_id' in firstBlog), true)
-})
+      assert.strictEqual('id' in firstBlog, true)
+      assert.strictEqual(!('_id' in firstBlog), true)
+    })
+  })
 
-test('POST requests should work as expected', async () => {
-  const testPost =
-  {
-    title: "Quit School",
-    author: "Primeagen",
-    url: "www.primeagen.com",
-    likes: 2,
-  }
+  describe('addition of new blogs', () => {
 
-  await api
-    .post('/api/blogs')
-    .send(testPost)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
+    test('works with valid blog', async () => {
+      const testPost =
+      {
+        title: "Quit School",
+        author: "Primeagen",
+        url: "www.primeagen.com",
+        likes: 2,
+      }
 
-  const response = await blogsInDb()
-  assert.strictEqual(response.length, initialBlogs.length + 1)
-})
+      await api
+        .post('/api/blogs')
+        .send(testPost)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
 
-test('POST requests with empty like will default to 0 ', async () => {
-  const testPost =
-  {
-    title: "Quit School",
-    author: "Primeagen",
-    url: "www.primeagen.com",
-  }
+      const response = await blogsInDb()
+      assert.strictEqual(response.length, initialBlogs.length + 1)
+    })
 
-  await api
-    .post('/api/blogs')
-    .send(testPost)
-    .expect(201)
+    test('empty like will default to 0 ', async () => {
+      const testPost =
+      {
+        title: "Quit School",
+        author: "Primeagen",
+        url: "www.primeagen.com",
+      }
 
-  const response = await blogsInDb()
+      await api
+        .post('/api/blogs')
+        .send(testPost)
+        .expect(201)
 
-  assert.strictEqual('likes' in response[initialBlogs.length - 1], true)
+      const response = await blogsInDb()
 
-  console.log(response[initialBlogs.length])
-  assert.strictEqual(response[initialBlogs.length].likes, 0)
-})
+      assert.strictEqual('likes' in response[initialBlogs.length - 1], true)
+      assert.strictEqual(response[initialBlogs.length].likes, 0)
+    })
 
-test('POST requests with invalid requests should return 400', async () => {
+    test('with invalid requests should return 400', async () => {
 
-  const testInvalidPost =
-  {
-    url: "www.primeagen.com",
-  }
+      const testInvalidPost =
+      {
+        url: "www.primeagen.com",
+      }
 
-  await api
-    .post('/api/blogs')
-    .send(testInvalidPost)
-    .expect(400)
-})
+      await api
+        .post('/api/blogs')
+        .send(testInvalidPost)
+        .expect(400)
+    })
+  })
 
-test('DELETE request with a valid id should return a 204', async () => {
-  const idToDelete = initialBlogs[0]._id
+  describe('Deletion of blog', () => {
 
-  await api
-    .delete(`/api/blogs/${idToDelete}`)
-    .expect(204)
+    test('valid id should return a 204', async () => {
+      const idToDelete = initialBlogs[0]._id
+
+      await api
+        .delete(`/api/blogs/${idToDelete}`)
+        .expect(204)
+    })
+  })
+
+  describe('Updating posts', () => {
+
+    test("with a valid request will be successful", async () => {
+      const idToUpdate = initialBlogs[0]._id
+
+      const updateBlog = {
+        author: "Vincent",
+        title: "Find a job at FAANG",
+        url: "www.thisisascam.com",
+        likes: 50
+      }
+
+      const response = await api
+        .put(`/api/blogs/${idToUpdate}`)
+        .send(updateBlog)
+
+      assert.strictEqual(response.body.likes, updateBlog.likes)
+    })
+  })
 })
 
 after(async () => {
