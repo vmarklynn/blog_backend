254e79b66b498a16f5852a7a132bcb9ddeec5029
Vincent Marklynn <56606238+vmarklynn@users.noreply.github.com>
2024-07-19 22:50:51 -0700

    Added PUT to update

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -21,9 +21,24 @@ blogsRouter.post('/', async (request, response, next) => {
 
 })
 
-blogsRouter.delete('/:id', async (request, response, next) => {
+blogsRouter.delete('/:id', async (request, response) => {
   await Blog.findByIdAndDelete(request.params.id)
   response.status(204).end()
+})
+
+blogsRouter.put('/:id', async (request, response) => {
+  const body = request.body
+  const newBlog = {
+    author: body.author,
+    title: body.title,
+    url: body.url,
+    likes: body.likes
+  }
+
+  const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, newBlog, { new: true })
+
+  response.json(updatedBlog)
+
 })
 
 module.exports = blogsRouter
