54ecac06f075d38aabbb76ec2a2923f995cc657c
Vincent Marklynn <56606238+vmarklynn@users.noreply.github.com>
2024-07-21 21:46:12 -0700

    Updated blogs, users controller to use populate

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,23 +1,32 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
   response.json(blogs)
 })
 
 blogsRouter.post('/', async (request, response, next) => {
   const body = request.body
 
+  // TODO: This is a rudimentary implementation that ALWAYS grabs the first user
+  const users = await User.find({})
+  const userToUpdate = users[0]
+
   const blog = new Blog({
     title: body.title,
     author: body.author,
     url: body.url,
-    likes: body.likes
+    likes: body.likes,
+    user: userToUpdate.id
   })
 
-  const res = await blog.save()
-  response.status(201).json(res)
+  const savedBlog = await blog.save()
+  userToUpdate.blogs = userToUpdate.blogs.concat(savedBlog._id)
+  await userToUpdate.save()
+
+  response.status(201).json(savedBlog)
 
 })
 
--- controllers/users.js
+++ controllers/users.js
@@ -3,7 +3,7 @@ const User = require('../models/user')
 const bcrypt = require('bcryptjs')
 
 usersRouter.get('/', async (request, response) => {
-  const users = await User.find({})
+  const users = await User.find({}).populate('blogs', { title: 1, url: 1, author: 1, likes: 1 })
   response.json(users)
 })
 
