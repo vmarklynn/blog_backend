2f80c3f23e617febeb1e03b7d5f62b22b23cafc5
Vincent Marklynn <56606238+vmarklynn@users.noreply.github.com>
2024-07-30 09:48:43 -0700

    Updated to handle Playwright testing

--- app.js
+++ app.js
@@ -25,6 +25,11 @@ app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
 
+if (process.env.NODE_ENV === "test") {
+  const testRouter = require('./controllers/testing')
+  app.use('/api/testing', testRouter)
+}
+
 app.use(validationError)
 app.use(unknownEndpoint)
 
